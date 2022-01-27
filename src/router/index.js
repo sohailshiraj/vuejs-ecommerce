600317d3a7a8072101d4b49b7e9a108b5ef2864f
Sohail Shiraj <sohail.shiraj@hotmail.com>
2022-01-27 00:23:26 -0500

    Home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Layout from "../components/Layout";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    component: Layout,
+    children:[
+      {
+        path: "/",
+        name: "Home",
+        component: Home,
+      }
+    ]
   },
   {
     path: "/about",
