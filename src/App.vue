529b8fc364bb6c967da66b33f125e6865744484d
Sohail Shiraj <sohail.shiraj@hotmail.com>
2022-01-30 15:48:09 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <component :is="resolveLayout">
+    <router-view></router-view>
+    <upgrade-to-pro></upgrade-to-pro>
+  </component>
 </template>
 
 <script>
-export default {
-  name: "App",
+import { computed } from '@vue/composition-api'
+import { useRouter } from '@/utils'
+import LayoutBlank from '@/layouts/Blank.vue'
+import LayoutContent from '@/layouts/Content.vue'
+import UpgradeToPro from './components/UpgradeToPro.vue'
 
-  data: () => ({
-    //
-  }),
-};
+export default {
+  components: {
+    LayoutBlank,
+    LayoutContent,
+    UpgradeToPro,
+  },
+  setup() {
+    const { route } = useRouter()
+
+    const resolveLayout = computed(() => {
+      // Handles initial route
+      if (route.value.name === null) return null
+
+      if (route.value.meta.layout === 'blank') return 'layout-blank'
+
+      return 'layout-content'
+    })
+
+    return {
+      resolveLayout,
+    }
+  },
+}
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,87 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Layout from "../components/Layout";
-import Home from "../views/Home.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-import Shop from "@/components/Shop.vue";
-import Product from "@/components/Product.vue";
-import Cart from "@/components/Cart.vue";
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    component: Layout,
-    children: [
-      {
-        path: "/",
-        name: "Home",
-        component: Home,
-      },
-      {
-        path: "/shop",
-        component: Shop,
-        name: "Shop",
-      },
-      {
-        path: "/product",
-        component: Product,
-        name: "Product",
-      },
-      {
-        path: "/cart",
-        component: Cart,
-        name: "Cart",
-      },
-    ],
+    path: '/',
+    redirect: 'dashboard',
   },
   {
-    path: "/login",
-    name: "Login",
-    component: Login,
+    path: '/products',
+    name: 'icons',
+    component: () => import('@/views/pages/Products.vue'),
   },
   {
-    path: "/register",
-    name: "Register",
-    component: Register,
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('@/views/dashboard/Dashboard.vue'),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/typography',
+    name: 'typography',
+    component: () => import('@/views/typography/Typography.vue'),
   },
-];
+  {
+    path: '/icons',
+    name: 'icons',
+    component: () => import('@/views/icons/Icons.vue'),
+  },
+  {
+    path: '/cards',
+    name: 'cards',
+    component: () => import('@/views/cards/Card.vue'),
+  },
+  {
+    path: '/simple-table',
+    name: 'simple-table',
+    component: () => import('@/views/simple-table/SimpleTable.vue'),
+  },
+  {
+    path: '/form-layouts',
+    name: 'form-layouts',
+    component: () => import('@/views/form-layouts/FormLayouts.vue'),
+  },
+  {
+    path: '/pages/account-settings',
+    name: 'pages-account-settings',
+    component: () => import('@/views/pages/account-settings/AccountSettings.vue'),
+  },
+  {
+    path: '/pages/login',
+    name: 'pages-login',
+    component: () => import('@/views/pages/Login.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '/pages/register',
+    name: 'pages-register',
+    component: () => import('@/views/pages/Register.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '/error-404',
+    name: 'error-404',
+    component: () => import('@/views/Error.vue'),
+    meta: {
+      layout: 'blank',
+    },
+  },
+  {
+    path: '*',
+    redirect: 'error-404',
+  },
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
